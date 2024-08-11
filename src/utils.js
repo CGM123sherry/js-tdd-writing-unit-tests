646bda282ece64fa248e4288553b0484dedc5e83
CGM123sherry <g.christinemurithi@gmail.com>
2024-08-11 07:29:31 +0300

    added further tests

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,34 @@
-// Your code here
-// src/utils.js
+// // Your code here
+// src / utils.js;
+// export function pointsForWord(word) {
+//   let points = 0;
+//   for (const char of word) {
+//     if (["a", "e", "i", "o", "u"].includes(char)) {
+//       points += 1;
+//     } else {
+//       points += 2;
+//     }
+//   }
+//   return points;
+// }
+// export function pointsForWord(word) {
+//   let points = 0;
+//   for (const char of word) {
+//     points += /[aeiou]/.test(char) ? 1 : 2;
+//   }
+//   return points;
+// }
+// it("handles uppercase and lowercase input", () => {
+//   const word = "tEsT";
+
+//   const points = pointsForWord(word);
+
+//   expect(points).toBe(7);
+// });
 export function pointsForWord(word) {
   let points = 0;
   for (const char of word) {
-    if (["a", "e", "i", "o", "u"].includes(char)) {
-      points += 1;
-    } else {
-      points += 2;
-    }
+    points += /[aeiou]/i.test(char) ? 1 : 2;
   }
   return points;
 }
